a8b689f020c25da846bc56e844a2332826d35b94
ThaysMrtns <thays.martins@nave.org.br>
2021-01-12 15:50:55 -0300

    Scaffold

--- frasesDia/lib/main.dart
+++ frasesDia/lib/main.dart
@@ -2,7 +2,29 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: App()
+    home: Scaffold(
+      appBar: AppBar( //Barra de navegação
+        title: Text("Instagram"),
+        backgroundColor: Colors.purple,
+      ),
+      body: Padding( //Corpo do app
+        padding: EdgeInsets.all(20),
+        child: Text("Coé meu amigo"),
+      ),
+      bottomNavigationBar: BottomAppBar( //Footer
+        color: Colors.purple[200],
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Row(
+            children: [
+              Text("T1"),
+              Text("T2"),
+              Text("T3")
+            ],
+          ),
+        )
+      ),
+    ),
   ));
   
 }
