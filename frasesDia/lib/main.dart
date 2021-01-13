e77fd14eb52a3b6b977a7e96d565e0bc6158fc40
ThaysMrtns <thays.martins@nave.org.br>
2021-01-13 11:42:38 -0300

    Alterando estado de variável

--- frasesDia/lib/main.dart
+++ frasesDia/lib/main.dart
@@ -2,22 +2,54 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: App()
+    home: HomeStateful()
   ));
   
 }
 
-class App extends StatelessWidget {
+// Aprendendo statefull widget
+// Tem sempre um estado inicial que pode ser alterado no processo
+class HomeStateful extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<HomeStateful> {
+  var _frase = "Que a força esteja com você";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return 
+      Scaffold(
       appBar: AppBar( //Barra de navegação
-        title: Text("Instagram"),
+        title: Text("Frases do dia"),
         backgroundColor: Colors.purple,
       ),
-      body: Padding( //Corpo do app
-        padding: EdgeInsets.all(20),
-        child: Text("Coé meu amigo"),
+      body: Center(
+        child:
+          Padding( //Corpo do app
+          padding: EdgeInsets.fromLTRB(20, 50, 20, 20),
+          child: 
+              Column(
+                children: [
+                  Text("Clique no botão para gerar uma frase"),
+                  RaisedButton(onPressed: (){
+                      setState(() {
+                        _frase = "Todo mundo odeia bugs";
+                      });
+                    },
+                    color: Colors.purple[300],
+                    textColor: Colors.white,
+                    child:
+                    const Text(
+                      "Gerar frase", 
+                      style:
+                      TextStyle(fontSize: 16)
+                    ),
+                  ),
+                  Text("$_frase"),
+                ], 
+            ),  
+          ),
       ),
       bottomNavigationBar: BottomAppBar( //Footer
         color: Colors.purple[200],
@@ -25,9 +57,7 @@ class App extends StatelessWidget {
           padding: EdgeInsets.all(15),
           child: Row(
             children: [
-              Text("T1"),
-              Text("T2"),
-              Text("T3")
+              Text("Rodapé"),
             ],
           ),
         )
